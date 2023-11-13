205a15d85c53c5a29c3722bebc5fac7531c59afc
RomaBortnik <bortnik25051998@gmail.com>
2023-11-13 19:21:05 +0200

    added global styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "modern-normalize";
+import { Global } from "@emotion/react";
+
+import { GlobalStyles } from "./styles/globalStyles";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Global styles={GlobalStyles} />
     <App />
   </React.StrictMode>
 );

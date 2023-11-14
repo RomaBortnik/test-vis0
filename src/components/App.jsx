ce31132c759824c295a84925e54dee7da4969c40
RomaBortnik <bortnik25051998@gmail.com>
2023-11-14 13:48:36 +0200

    added google maps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,20 @@
+import { useJsApiLoader } from "@react-google-maps/api";
+import { Map } from "./Map";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const center = {
+  lat: 49.5290848,
+  lng: 26.141698,
+};
+
 const App = () => {
-  return <h1>App</h1>;
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: API_KEY,
+  });
+
+  return isLoaded ? <Map center={center} /> : <h2>Loading</h2>;
 };
 
 export default App;
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,49 @@
+import { GoogleMap } from "@react-google-maps/api";
+import { useCallback, useRef } from "react";
+import { MapWrapper } from "./Map.styled";
+
+const containerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+const defaultOptions = {
+  panControl: true,
+  zoomControl: true,
+  mapTypeControl: false,
+  scaleControl: false,
+  streetViewControl: false,
+  rotateControl: false,
+  clickableIcons: false,
+  keyboardShortcuts: false,
+  scrollwheel: true,
+  disableDoubleClickZoom: true,
+  fullscreenControl: true,
+};
+
+const Map = ({ center }) => {
+  const mapRef = useRef(undefined);
+
+  const onLoad = useCallback(function callback(map) {
+    mapRef.current = map;
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
+    mapRef.current = undefined;
+  }, []);
+
+  return (
+    <MapWrapper>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        options={defaultOptions}
+      ></GoogleMap>
+    </MapWrapper>
+  );
+};
+
+export { Map };
